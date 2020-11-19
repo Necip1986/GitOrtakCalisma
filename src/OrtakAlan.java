cc9ca56c1b9c2fb8948b06b2ef9ddb9e629fa7ad
Necip1986 <74536035+Necip1986@users.noreply.github.com>
2020-11-19 22:09:50 +0200

    Update OrtakAlan.java

--- src/OrtakAlan.java
+++ src/OrtakAlan.java
@@ -3,6 +3,11 @@ public class OrtakAlan {
 
 	public static void main(String[] args) {
 		System.out.println("09:43 - Selamlar");
+		
+		int c=15;
+		for(int i=0; i<c; i++){
+			System.out.println(i+" ");
+		}
 
 	}
 
